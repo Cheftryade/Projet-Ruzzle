e0aca330f92876be2fe1c125604b214674bf71a6
keryann <spi2097@ic2s122-10.univ-lemans.fr>
2015-11-16 16:35:46 +0100

    ajout du main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define N 4
+
+int main() {
+    Mat[N][N];
+    creaMatrice();
+    for(i=0;i<N;i++) {
+        for(j=0;i<N;j++) {
+            /* fonction qui vérifie que les lettres qu'on regarde sont un début de mot
+             * si ce n'est pas le cas on sort de la fonction
+             * sinon on continue à regarder avec la lettre suivante une fois qu'on est en bout de mot on écrit
+             * le mot dans un fichier texte
+             */
+        }
+    }
+}
